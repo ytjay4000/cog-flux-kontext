ed6300730d0b292b9229cc7a2a890ed99d20fb9b
andreasjansson <andreas@replicate.ai>
2025-05-28 17:30:03 +0200

    Remove unused imports

--- predict.py
+++ predict.py
@@ -1,10 +1,7 @@
 import os
-import sys
-import subprocess
 import torch
 from PIL import Image
 from cog import BasePredictor, Path, Input
-from transformers import pipeline
 
 from flux.sampling import denoise, get_schedule, prepare_kontext, unpack
 from flux.util import (
